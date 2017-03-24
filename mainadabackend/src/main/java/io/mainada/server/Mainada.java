50b801dbf611e6ff92fbce5aabc0c68454f42212
Gabriel Pinto <gabriel.pinto@mindera.com>
2017-03-24 12:22:09 +0000

    Um servidor que diz olá

---
+++ mainadabackend/src/main/java/io/mainada/server/Mainada.java
@@ -0,0 +1,13 @@
+package io.mainada.server;
+
+import io.reactivex.netty.protocol.http.server.HttpServer;
+
+import static rx.Observable.just;
+
+public class Mainada {
+    public static void main(final String[] args) {
+        HttpServer.newServer(8080).start((req, resp) -> resp.writeString(just("Hello World!")));
+
+
+    }
+}

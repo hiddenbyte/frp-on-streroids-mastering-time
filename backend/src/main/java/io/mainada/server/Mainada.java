d519f1ad973e62a1dbde8e5c4f57123c5e04eaca
Gabriel Pinto <gabriel.pinto@mindera.com>
2017-03-26 14:18:09 +0100

    make server await for shutdown

--- backend/src/main/java/io/mainada/server/Mainada.java
+++ backend/src/main/java/io/mainada/server/Mainada.java
@@ -1,11 +1,16 @@
 package io.mainada.server;
 
+import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.server.HttpServer;
 
 import static rx.Observable.just;
 
 public class Mainada {
     public static void main(final String[] args) {
-        HttpServer.newServer(8080).start((req, resp) -> resp.writeString(just("Hello World!")));
+        final HttpServer<ByteBuf, ByteBuf> server = HttpServer.newServer(8080);
+
+        server.start((req, resp) -> resp.writeString(just("Hello World!")));
+
+        server.awaitShutdown();
     }
 }

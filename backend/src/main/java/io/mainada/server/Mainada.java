158c9193d08723f511ef4dfc4b6a69c3e467a20d
Gabriel Pinto <gabriel.pinto@mindera.com>
2017-03-26 14:08:52 +0100

    Use gradle instead of maven

--- backend/src/main/java/io/mainada/server/Mainada.java
+++ backend/src/main/java/io/mainada/server/Mainada.java
@@ -7,7 +7,5 @@ import static rx.Observable.just;
 public class Mainada {
     public static void main(final String[] args) {
         HttpServer.newServer(8080).start((req, resp) -> resp.writeString(just("Hello World!")));
-
-
     }
 }
